f61c2bd8e71e82c419f4a4f591b9b64686a81d1f
timeeftink-tf <tim.eeftink@timformatie.nl>
2023-07-03 16:44:43 +0200

    add workflow to perform tests on release

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,6 +1,8 @@
 
+# TODO: find way to deal with testing functions which need a secret
+
 test_that("get_return_url() returns correctly encoded url", {
-  api_info <- get_api_info(Sys.getenv("APP_ENV"))
+  api_info <- list(base_url = Sys.getenv("PULSE_BASE_URL"))
   return_url <- get_return_url(
     api_info,
     patient_id = Sys.getenv("PATIENT_ID"),
